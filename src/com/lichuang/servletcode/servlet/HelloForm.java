36fffd4b81c4b21afce28a4da93a89aa07a05442
li-chuang <278747148@qq.com>
2017-04-15 23:31:35 +0800

    Create HelloForm.java

---
+++ src/com/lichuang/servletcode/servlet/HelloForm.java
@@ -0,0 +1,59 @@
+package com.lichuang.servletcode.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloForm
+ */
+@WebServlet("/HelloForm")
+public class HelloForm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloForm() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String title = "使用 GET 方法读取表单数据";
+		
+		String name = new String(request.getParameter("name").getBytes("ISO8859-1"),"UTF-8");
+		String url = request.getParameter("url");
+		
+		String docType = "<!DOCTYPE html> \n";
+		out.println(docType +
+		    "<html>\n" +
+		    "<head><title>" + title + "</title></head>\n" +
+		    "<body bgcolor=\"#f0f0f0\">\n" +
+		    "<h1 align=\"center\">" + title + "</h1>\n" +
+		    "<ul>\n" +
+		    "  <li><b>站点名</b>："
+		    + name + "\n" +
+		    "  <li><b>网址</b>："
+		    + url + "\n" +
+		    "</ul>\n" +
+		    "</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
