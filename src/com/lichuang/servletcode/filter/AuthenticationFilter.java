48b193722819d8c9ec3a1551e48f8fa0eb3791c4
li-chuang <278747148@qq.com>
2017-04-25 23:36:44 +0800

    Update AuthenticationFilter.java

--- src/com/lichuang/servletcode/filter/AuthenticationFilter.java
+++ src/com/lichuang/servletcode/filter/AuthenticationFilter.java
@@ -11,6 +11,17 @@ import javax.servlet.annotation.WebFilter;
 
 /**
  * Servlet Filter implementation class Authentication
+ * 
+ * @WebFilter 的属性
+ * filterName 		String 	 		指定过滤器的 name 属性，等价于 <filter-name>
+ * value 			String[] 		该属性等价于 urlPatterns 属性。但是两者不应该同时使用。
+ * urlPatterns 		String[] 		指定一组过滤器的 URL 匹配模式。等价于 <url-pattern> 标签。
+ * servletNames 	String[] 		指定过滤器将应用于哪些 Servlet。取值是 @WebServlet 中的 name 属性的取值，或者是 web.xml 中 <servlet-name> 的取值。
+ * dispatcherTypes 	DispatcherType 	指定过滤器的转发模式。具体取值包括： ASYNC、ERROR、FORWARD、INCLUDE、REQUEST。
+ * initParams 		WebInitParam[] 	指定一组过滤器初始化参数，等价于 <init-param> 标签。
+ * asyncSupported 	boolean 		声明过滤器是否支持异步操作模式，等价于 <async-supported> 标签。
+ * description 		String 			该过滤器的描述信息，等价于 <description> 标签。
+ * displayName 		String 			该过滤器的显示名，通常配合工具使用，等价于 <display-name> 标签。
  */
 @WebFilter("/*")
 public class AuthenticationFilter implements Filter {
