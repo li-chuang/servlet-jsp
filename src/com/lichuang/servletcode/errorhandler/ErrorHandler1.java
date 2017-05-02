2bd12e6f6fd88387d65699fc9b258f5eba476b62
li-chuang <278747148@qq.com>
2017-05-02 22:45:28 +0800

    Update ErrorHandler1.java

--- src/com/lichuang/servletcode/errorhandler/ErrorHandler1.java
+++ src/com/lichuang/servletcode/errorhandler/ErrorHandler1.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ErrorHandler1
  */
-@WebServlet("/ErrorHandler1")
+@WebServlet("/ErrorHandler")
 public class ErrorHandler1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
